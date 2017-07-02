d45c1dbfc1bad064e9de5594b836dcab21382f64
unknown <you@example.com>
2017-07-02 19:21:51 +0300

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,38 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+    if(err){
+        return console.log("Unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    db.collection('Users').find({name:"Evgen"}).toArray().then((docs)=>{
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2))
+    },(err)=>{
+        console.log("Unable to find user");
+    });
+
+
+
+    //db.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
