a8659b523d10d1a6a6030237bd862ba1fa71cf08
unknown <you@example.com>
2017-07-04 23:10:55 +0300

    Test DELETE /todos/:id ruote

--- server/server.js
+++ server/server.js
@@ -55,6 +55,20 @@ app.get('/todos/:id', (req, res)=>{
 
 });
 
+app.delete('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){return res.status(404).send()};
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        };
+        res.status(200).send({todo});
+    }, (err)=>{
+        return res.status(400).send("Error heppens: ", err)
+    })
+
+})
+
 app.listen(port, ()=>{
     console.log('Server stared on port ', port)
 });
